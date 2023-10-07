fa90f7ea97da95e31c6e7bbbea0c4e735e8ca428
AIyueyixz <xzzsryouxiwang@163.com>
2023-10-07 12:51:06 +0800

    增加了指针面试题（加深了一级二级三级指针以及++、--运算符的理解）

--- test_7_21/test_7_21/test_7_21.c
+++ test_7_21/test_7_21/test_7_21.c
@@ -2531,3 +2531,35 @@
 //	return 0;
 //}
 
+//int main()
+//{
+//	int aa[2][5] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//	printf("%d,%d\n", *(ptr1 - 1), *(ptr2 - 1));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* a[] = { "work","at","allibaba" };
+//	char** pa = a;
+//	pa++;
+//	printf("%s\n", *pa);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//
+//	printf("%s\n", **++cpp);//这个++cpp会改变cpp的值，从而后面的cpp都要从新的位置开始考虑
+//	printf("%s\n", *-- * ++cpp + 3);//++,--运算符会改变cpp的值，使得它不再是原来的值
+//	printf("%s\n", *cpp[-2]+3);
+//	printf("%s\n", cpp[-1][-1]+1);
+//
+//	return 0;
+//}
+
